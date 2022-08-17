d7d97aec1297b80c947bca8a388ffff906c018b5
rickydayday <35758683+rickydayday@users.noreply.github.com>
2022-08-17 21:38:10 +0300

    Add files via upload

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,35 @@ const users= [
 	
 	"firstname":"john",
 	"lastname": "Doe",
-	"Age": 35
+	"age": 35
+},
+
+{
+
+
+	"firstname":"Jane Doe",
+	"lastname": "Doe",
+	"age": 35
+
 }
 
 ];
 
 
 
+
+
 router.get('/', (req,res)=>{
 	res.send(users);
 })
+
+router.post('/', (req,res)=>{
+
+	const user = req.body;
+
+
+	
+
+});
 
 export default router;
